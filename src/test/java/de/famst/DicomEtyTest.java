96ebda9547200f79b45cfcb324eff037c76e7f3e
Jens Stockhausen <jens@famst.de>
2016-11-27 20:17:17 +0100

    testing thymleaves

--- src/test/java/de/famst/DicomEtyTest.java
+++ src/test/java/de/famst/DicomEtyTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,7 @@ import static org.hamcrest.Matchers.*;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class DicomEtyTest
 {
     @Inject
---
+++ src/test/java/de/famst/dcm/LoadDICOMFromJSON.java
@@ -0,0 +1,28 @@
+package de.famst.dcm;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.json.JSONReader;
+
+import javax.json.Json;
+import java.io.InputStream;
+
+/**
+ * Created by jens on 27/11/2016.
+ */
+public class LoadDICOMFromJSON
+{
+    public LoadDICOMFromJSON()
+    {
+    }
+
+    public Attributes fromResource(String resource)
+    {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(resource);
+
+        JSONReader reader = new JSONReader(Json.createParser(inputStream));
+
+        return reader.readDataset(null);
+    }
+
+}
