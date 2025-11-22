249e92deca38f4ce2e4af0c40a2ea94d27019758
jensstockhausen <jens@famst.de>
2025-11-22 17:05:10 +0100

    update database configuration and enhance integration tests

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # mupacs
-[![Build Status](https://travis-ci.org/jensstockhausen/mupacs.svg?branch=master)](https://travis-ci.org/jensstockhausen/mupacs) 
-[![Known Vulnerabilities](https://snyk.io/test/github/jensstockhausen/mupacs/badge.svg)](https://snyk.io/test/github/jensstockhausen/mupacs)
 
-## Travis Builds 
 
-https://travis-ci.org/jensstockhausen/mupacs
 
--- src/test/java/de/famst/MupacsApplicationTests.java
+++ src/test/java/de/famst/MupacsApplicationTests.java
@@ -2,14 +2,24 @@ package de.famst;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Integration test for the MuPACS application.
+ * Verifies that the Spring application context loads successfully with test configuration.
+ */
 @SpringBootTest
+@ActiveProfiles("test")
 public class MupacsApplicationTests {
 
+	/**
+	 * Tests that the Spring application context loads successfully.
+	 * This verifies all beans can be created and autowired properly.
+	 */
 	@Test
 	public void contextLoads()
 	{
-
+		// Context loads successfully if this test passes
 	}
 
 }
